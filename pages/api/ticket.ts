b4859ac09e5ab74dfb4a7c84b9ff23faff804134
Asad Hayat <asadhayat.1068@gmail.com>
2023-08-24 02:21:46 +0500

    Postgres integration

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,5 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { db } from '@vercel/postgres';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ name: 'John Doe' });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const _resp = await addData(req.body);
+        res.status(200).json(_resp);
+    }
+    if (req.method === 'GET') {
+        const client = await db.connect();
+        const data = await client.sql`SELECT * FROM tickets`;
+        res.status(200).json(data);
+    }
+}
+
+const addData = async (data: any) => {
+    const client = await db.connect();
+    const payload = JSON.stringify(data);
+    await client.sql`INSERT INTO tickets (data) VALUES (${payload})`;
+    const result = await client.sql`SELECT * FROM tickets`;
+    return {"success": true};
 }
