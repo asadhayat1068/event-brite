9f67d13a057255f2a371f98e048740b4b8c8cc4c
Asad Hayat <asadhayat.1068@gmail.com>
2023-08-31 16:04:08 +0500

    updated ticket

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { db } from '@vercel/postgres';
+import { processTicket } from "../services/ticketProcessor";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
@@ -7,16 +8,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).json(_resp);
     }
     if (req.method === 'GET') {
+        // await processTicket();
         const client = await db.connect();
-        const data = await client.sql`SELECT * FROM tickets`;
-        res.status(200).json(data.rows);
+       const data = await client.sql`SELECT * FROM tickets_eb`;
+        res.status(200).json({data});
     }
 }
 
 const addData = async (data: any) => {
     const client = await db.connect();
     const payload = JSON.stringify(data);
-    await client.sql`INSERT INTO tickets (data) VALUES (${payload})`;
-    const result = await client.sql`SELECT * FROM tickets`;
+    await client.sql`INSERT INTO tickets_eb (data) VALUES (${payload})`;
+    const result = await client.sql`SELECT * FROM tickets_eb`;
     return {"success": true};
 }
