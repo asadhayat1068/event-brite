72dcc4c6bbc8d3b294e96fbec7195d30a07e34f0
Asad Hayat <asadhayat.1068@gmail.com>
2023-08-31 16:08:12 +0500

    unused import

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { db } from '@vercel/postgres';
-import { processTicket } from "../services/ticketProcessor";
+// import { processTicket } from "../services/ticketProcessor";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
